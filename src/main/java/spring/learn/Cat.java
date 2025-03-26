6ffc2f5c4db58f365b530a4adc7b906d2dce6d33
root <root@kali.com>
2025-03-26 23:40:42 +0300

    day 1 - dependency enjection changes using constructor

--- src/main/java/spring/learn/Cat.java
+++ src/main/java/spring/learn/Cat.java
@@ -1,6 +1,10 @@
 package spring.learn;
 
 public class Cat implements Pet{
+
+    public Cat() {
+        System.out.println("Cat bean is created");
+    }
     @Override
     public void say() {
         System.out.println("mew");
--- src/main/java/spring/learn/Test3.java
+++ src/main/java/spring/learn/Test3.java
@@ -7,9 +7,9 @@ public class Test3 {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Pet pet = context.getBean("myPet", Pet.class);
+//        Pet pet = context.getBean("myPet", Pet.class);
 //        Pet pet = new Dog();
-        Person person = new Person(pet);
+        Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
 
         context.close();
