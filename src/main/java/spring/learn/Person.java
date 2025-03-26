b4b56c8fd8dcb2777041b76d7e7295f29d2d7d8b
root <root@kali.com>
2025-03-26 23:34:08 +0300

    day 1 - dependency injection

---
+++ src/main/java/spring/learn/Person.java
@@ -0,0 +1,14 @@
+package spring.learn;
+
+public class Person {
+    private Pet pet;
+
+    public Person(Pet pet) {
+        this.pet = pet;
+    }
+
+    public void callYourPet() {
+        System.out.println("Hello, my lovely Pet");
+        pet.say();
+    }
+}
---
+++ src/main/java/spring/learn/Test3.java
@@ -0,0 +1,17 @@
+package spring.learn;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test3 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Pet pet = context.getBean("myPet", Pet.class);
+//        Pet pet = new Dog();
+        Person person = new Person(pet);
+        person.callYourPet();
+
+        context.close();
+    }
+}
